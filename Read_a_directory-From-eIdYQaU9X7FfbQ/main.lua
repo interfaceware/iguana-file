e4dfcb49c4c63b095ce4784137a1523ab4e6909c
admin <admin>
2016-01-30 18:15:44 +0000

    Export: Simplified the read a directory example significantly.

--- Read_a_directory-From-eIdYQaU9X7FfbQ/main.lua
+++ Read_a_directory-From-eIdYQaU9X7FfbQ/main.lua
@@ -1,53 +1,26 @@
 
 -- This example shows the calls to iterate through a directory.
--- The one complication is that the wild card matching expression
--- has to be *.* for windows and * for OS X, Unix and Linux.
+-- The timestamp information is in unix epoch time - number of seconds since 1971
+-- The os.ts.date function is helpful for converting this to human readable formats.
+-- See http://help.interfaceware.com/api/#os_fs_glob for more information on os.fs.glob
 
 function main()
    -- We will iterate through the default directory
    local Dir = iguana.workingDir()
    -- We'll make a list the files/dirs we find
    local List = {}
-   -- Glob matching pattern 
-   -- is *.* for Windows, * for everything else.
-   local Glob = GlobForOs()
-   
-   for Name, FileInfo in os.fs.glob(Dir..Glob) do
+   for Name, FileInfo in os.fs.glob(Dir..'*') do
       trace(Name)
       List[Name] = FileInfo
       -- FileInfo has interesting stats on each file
       trace("Size  = "..FileInfo.size)
+      -- Formating the time for readability
+      os.ts.date("%c", FileInfo.ctime)
+      -- Putting a ! in gives us UTC time
+      os.ts.date("!%c", FileInfo.ctime)
       if (FileInfo.isdir) then
-         ShowDir(Name, FileInfo)
-      else
-         ShowFile(Name, FileInfo)
+         trace("It is a dir")
       end
    end
    trace(List)
-end
-
-
-function ShowDir(Name, Info)
-   -- This is the create time
-   trace(Info.ctime)
-   -- It's unix epoch time
-   -- number of seconds since
-   -- 1971.
-   -- We can convert it into a
-   -- string using os.ts.date
-   os.ts.date("%c", Info.ctime)
-   -- Putting a ! in gives us
-   -- UTC time
-   os.ts.date("!%c", Info.ctime)
-   -- Info.size can be interesting
-   trace(Info.size) 
-end
-
-
-function ShowFile(Name, FileInfo)
-      
-end
-
-function GlobForOs()
-   return '*'
 end
