1d54a2ab56d38b5bae4a5163b053f380e2a5fae7
admin <paul.gannon@interfaceware.com>
2016-04-13 13:08:01 +0000

    Export: Putting in documentation links.

--- Read_a_directory-From-eIdYQaU9X7FfbQ/main.lua
+++ Read_a_directory-From-eIdYQaU9X7FfbQ/main.lua
@@ -1,26 +1,31 @@
-
 -- This example shows the calls to iterate through a directory.
 -- The timestamp information is in unix epoch time - number of seconds since 1971
 -- The os.ts.date function is helpful for converting this to human readable formats.
--- See http://help.interfaceware.com/api/#os_fs_glob for more information on os.fs.glob
+-- See http://help.interfaceware.com/v6/read-a-directory
+
+-- for more information on os.fs.glob, see:
+-- http://help.interfaceware.com/api/#os_fs_glob
 
 function main()
-   -- We will iterate through the default directory
+   -- Get the working directory
    local Dir = iguana.workingDir()
-   -- We'll make a list the files/dirs we find
+   -- Iterate through the default directory
+   -- to make a list of the files/dirs we find
    local List = {}
    for Name, FileInfo in os.fs.glob(Dir..'*') do
       trace(Name)
-      List[Name] = FileInfo
-      -- FileInfo has interesting stats on each file
+      -- Add a list entry for each file
+      List[Name] = FileInfo      
+      -- Trace a few interesting stats FileInfo has on each file
       trace("Size  = "..FileInfo.size)
       -- Formating the time for readability
       os.ts.date("%c", FileInfo.ctime)
-      -- Putting a ! in gives us UTC time
+      -- Adding a ! gives us UTC time
       os.ts.date("!%c", FileInfo.ctime)
       if (FileInfo.isdir) then
          trace("It is a dir")
       end
    end
+   -- View the list we created
    trace(List)
 end
